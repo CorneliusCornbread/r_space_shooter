f4431ac47ab61e3a813b88779b9063a47f8f4a1e
CorneliusCornbread <22rosenj@elmbrookstudents.org>
2020-03-04 11:29:49 -0600

    agh

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 extern crate amethyst;
-use amethyst::prelude::*;
-use amethyst::winit::{Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+
+use amethyst::{
+    core::transform::TransformBundle,
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+
+    winit::
+    {
+        Event, 
+        KeyboardInput, 
+        VirtualKeyCode, 
+        WindowEvent
+    },
+
+    utils::application_root_dir,
+};
+
 
 struct GameState;
 
 fn main() -> amethyst::Result<()> 
 {
-    let assets_dir = "assets/";
+    let app_root = application_root_dir()?;
+    let assets_dir = app_root.join("assets");
+    let display_config = assets_dir.join("config/display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config)
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()),
+    )?;
+
     let mut game = Application::new(assets_dir, GameState, GameDataBuilder::default())?;
     game.run();
     Ok(())
